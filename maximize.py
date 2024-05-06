d557c0ef96ec9f2d5fc42d592f4a392126dbdf85
gautam0kr <gautamkr.2409@gmail.com>
2024-05-06 15:59:30 +0530

    corrected a program

--- maximize.py
+++ maximize.py
@@ -1,13 +1,37 @@
-k, m=map(int, input().split())
-max=[]
-for _ in range(k):
-    temp=list(map(int, input().split()))
+#code fails in various test cases
+# k, m=map(int, input().split())
+# maxnum=[]
+# for _ in range(k):
+#     temp=list(map(int, input().split()))
     
-    temp.pop(0)
-   
-    temp.sort()
-    max.append(temp[-1])
-res=0
-for num in max:
-    res+=(num ** 2)
-print(res % m)
+#     temp.pop(0)
+#     temp.sort()
+#     maxnum.append(temp[-1])
+# res=0
+# for num in maxnum:
+#     res+=(num ** 2)
+# print(res % m)
+
+
+
+import itertools
+
+def maximize_value(k, m, lists):
+    max_value = 0
+
+    # Generate all possible combinations of elements from the lists
+    for combination in itertools.product(*lists):
+        # Calculate the value of the expression
+        value = sum(x ** 2 for x in combination) % m
+        max_value = max(max_value, value)
+
+    return max_value
+
+# Read input
+k, m = map(int, input().split())
+lists = [list(map(int, input().split()))[1:] for _ in range(k)]
+
+# Calculate and print the maximum value
+print(maximize_value(k, m, lists))
+
+
